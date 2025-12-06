9cabdc6605933413e2e2a0deb84a31309730301d
Diego Reidel <diegoreidel@gmail.com>
2025-12-06 13:42:17 -0300

    Day 6

--- 06-homework/homework.go
+++ 06-homework/homework.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	input, err := io.ReadFile("./input/test.txt")
+	input, err := io.ReadFile("./input/input.txt")
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 	}
@@ -27,7 +27,6 @@ func firstPuzzle(grid [][]string) {
 	var aggregation int64
 
 	for col := 0; col < cols; col++ {
-
 		var answer int64
 		answer = 0
 		operation := operations[col]
@@ -51,82 +50,57 @@ func firstPuzzle(grid [][]string) {
 }
 
 func secondPuzzle(input []string) {
+	operations := input[len(input)-1]
 
-	numberOfLines := 0
-	var sizes []int
 	largestSize := 0
 
 	for _, line := range input {
-		numberOfLines++
 		length := len(line)
-		sizes = append(sizes, length)
 		if length > largestSize {
 			largestSize = length
 		}
 	}
 
 	numbers := make([]string, largestSize)
-	operations := ""
 
 	for col := largestSize - 1; col >= 0; col-- {
-		for row := 0; row < numberOfLines; row++ {
+		for row := 0; row < len(input)-1; row++ {
 			if col < len(input[row]) {
 				char := input[row][col]
-				if char == '+' || char == '*' {
-					operations = string(char) + operations
-				} else if char != ' ' {
+				if char != ' ' {
 					numbers[col] += string(char)
 				}
 			}
 		}
 	}
 
-	var noSpaces []string
-	for _, number := range numbers {
-		if number != "" {
-			noSpaces = append(noSpaces, number)
-		}
-	}
-
-	fmt.Println("The numbers are: ", noSpaces)
-	fmt.Println("Operations are: ", operations)
-
-	values := len(input) - 1
-
-	fmt.Println("The the number of values per operation is : ", values)
-
 	var operation uint8
 	var answer int64
-	var aggreation int64
+	var aggregation int64
 
-	for i, number := range noSpaces {
+	for i, number := range numbers {
 		value, _ := strconv.Atoi(number)
-
-		if i%values == 0 {
-			fmt.Println("The answer for group ", i/values, "is", answer)
-			aggreation += answer
-			operation = operations[i/values]
-
+		if i < len(operations) && operations[i] != ' ' {
+			operation = operations[i]
 			if operation == '*' {
 				answer = 1
-			} else {
-				answer = 0
 			}
 		}
 
-		if operation == '+' {
-			answer += int64(value)
-		} else if operation == '*' {
-			answer *= int64(value)
+		if len(strings.TrimSpace(number)) > 0 {
+			if operation == '+' {
+				answer += int64(value)
+			} else if operation == '*' {
+				answer *= int64(value)
+			}
+		} else {
+			aggregation += answer
+			answer = 0
 		}
 	}
 
-	fmt.Println("The answer for the last group is", answer)
-
-	aggreation += answer
-
-	fmt.Println("The answer for the second puzzle is: ", aggreation)
-
+	aggregation += answer
+	fmt.Println("The answer for the second puzzle is: ", aggregation)
 }
 
 func buildGrid(input []string) [][]string {
